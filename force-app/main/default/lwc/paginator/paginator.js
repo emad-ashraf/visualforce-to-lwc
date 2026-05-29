fb5a058d1aa6fa9a89ecf4196ba4fbb2fff72668
Serpent Org-to-Org Deployment <deployment@serpent.local>
2026-05-29 09:44:38 +0300

    chore: sync target org metadata

--- force-app/main/default/lwc/paginator/paginator.js
+++ force-app/main/default/lwc/paginator/paginator.js
@@ -1,23 +1,41 @@
 import { LightningElement, api } from 'lwc';
 
-export default class Paginator extends LightningElement {
-    @api previousButtonDisabled;
-    @api nextButtonDisabled;
-    @api currentPage = 1;
-    @api totalPages = 1;
+const MAX_ITEM_OFFSET = 2000;
 
-    get statusLabel() {
-        return `Showing ${this.currentPage} of ${this.totalPages}`;
-    }
+export default class Paginator extends LightningElement {
+    /** The current page number. */
+    @api pageNumber;
+
+    /** The number of items on a page. */
+    @api pageSize;
+
+    /** The total number of items in the list. */
+    @api totalItemCount;
 
     handlePrevious() {
-    // emit prev-page event
-    this.dispatchEvent(
-        new CustomEvent('paginateprevious')
-    );
+        this.dispatchEvent(new CustomEvent('previous'));
     }
 
     handleNext() {
-        this.dispatchEvent(new CustomEvent('paginatenext'));
+        this.dispatchEvent(new CustomEvent('next'));
+    }
+
+    get currentPageNumber() {
+        return this.totalItemCount === 0 ? 0 : this.pageNumber;
+    }
+
+    get isNotFirstPage() {
+        return this.pageNumber > 1;
+    }
+
+    get isNotLastPage() {
+        return (
+            this.pageNumber < this.totalPages &&
+            this.pageNumber * this.pageSize < MAX_ITEM_OFFSET
+        );
+    }
+
+    get totalPages() {
+        return Math.ceil(this.totalItemCount / this.pageSize);
     }
 }
